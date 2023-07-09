dc7fc874365494d690fbb4bbe0d632334b547ba1
Harsh Patel <harshp4106@gmail.com>
2023-07-09 14:58:52 +0530

    files

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,17 @@ from typing import Any
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path)-> ConfigBox:
-    """
+    """reads yaml file and returns
+
+    Args:
+        path_to_yaml (str): path like input
+
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: ConfigBox type
     """
 
     try:
@@ -21,5 +31,36 @@ def read_yaml(path_to_yaml: Path)-> ConfigBox:
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty") 
+    except Exception as e:
+        raise e
 
-        
+
+@ensure_annotations
+def create_directories(path_to_directories: List, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directories (list): list of path of directories
+        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+    """
+
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")  
+
+
+@ensure_annotations
+def get_size(path: Path)-> str:
+
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+
+    size_in_kb = round(os.path.get_size(Path)/1024)
+    return f"~ {size_in_kb} KB"
