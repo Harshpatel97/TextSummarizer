6a33c600269f8a022cb3e7571fe02b4eb2420776
Harsh Patel <harshp4106@gmail.com>
2023-07-13 11:49:38 +0530

    data ingestion notebook added

---
+++ src/entity/__init__.py
@@ -0,0 +1,12 @@
+## Entity -- return type of a function
+## these will return the values, so if we want to change some thing in future we just have to make change config.yaml.
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path 
